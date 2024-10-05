c783918c56866551416720c33238411f2500405b
Mateus Araujo <rb.mateus.araujo@gmail.com>
2024-10-05 18:26:09 -0300

    add assets files to display the colors correctly

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -35,7 +35,7 @@ struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
         CardView(scrum: scrum)
-            .background(.yellow)
+            .background(scrum.theme.mainColor)
             .previewLayout(.fixed(width: 400, height: 60))
     }
 }
