d900c5bc3632f33709897b541557744e92a43c55
Mateus Araujo <rb.mateus.araujo@gmail.com>
2024-10-05 19:30:25 -0300

    Displaying data in a list: create ScrumsView component list of CardView

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Mateus Araújo on 05/10/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
