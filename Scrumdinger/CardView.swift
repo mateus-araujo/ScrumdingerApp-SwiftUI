9bb8f59c7a36dee721d9adaedc425fd488364c28
Mateus Araujo <rb.mateus.araujo@gmail.com>
2024-10-05 18:04:57 -0300

    create CardView

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,41 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Mateus Araújo on 05/10/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.4")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meetinga")
+                    .labelStyle(.trailingIcon)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(.yellow)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
