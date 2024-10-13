2c071125f6febd4b3dae5f695b216924660196cb
Mateus Araujo <rb.mateus.araujo@gmail.com>
2024-10-13 11:16:41 -0300

    Managing state and life cycle: Create an overlay view

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct DetailView: View {
     @Binding var scrum: DailyScrum
-    
+
     @State private var editingScrum = DailyScrum.emptyScrum
     @State private var isPresentingEditView = false
     
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                NavigationLink(destination: MeetingView()) {
+                NavigationLink(destination: MeetingView(scrum: $scrum)) {
                     Label("Start Meeting", systemImage: "timer")
                         .font(.headline)
                         .foregroundColor(.accentColor)
@@ -69,7 +69,6 @@ struct DetailView: View {
                         }
                     }
             }
-            
         }
     }
 }
