630fc1d68a948fda9080b2dfb67d887292d227af
Mateus Araujo <rb.mateus.araujo@gmail.com>
2024-10-13 18:30:38 -0300

    Persisting data: Save data in inactive state

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
+    
+    let saveAction: () -> Void
     
     var body: some View {
         NavigationStack {
@@ -35,9 +38,12 @@ struct ScrumsView: View {
                 isPresentingNewScrumView: $isPresentingNewScrumView
             )
         }
+        .onChange(of: scenePhase) {
+            if scenePhase == .inactive { saveAction() }
+        }
     }
 }
 
 #Preview {
-    ScrumsView(scrums: .constant(DailyScrum.sampleData))
+    ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
 }
